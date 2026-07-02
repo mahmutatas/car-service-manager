313e1361bbaa8894550c928c36f319f689258d84
Mahmut Ataş <mahmut_atas03@hotmail.com>
2026-07-02 16:36:40 +0300

    Ilk commit

---
+++ src/main/java/com/rabam/carservice/CarServiceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rabam.carservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarServiceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabam/carservice/CarServiceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabam.carservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
